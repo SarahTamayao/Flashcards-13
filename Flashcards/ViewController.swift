81bb58bf6273b158fcaf7431abc94764a2ce8a19
Hamed Polo <hamedpolo62@gmail.com>
2021-03-31 22:02:31 -0400

    Add files via upload
    fixed final

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -24,6 +24,8 @@ class ViewController: UIViewController {
     //Current flashcard index
     var currentIndex = 0
     
+    var correctAnswerButton: UIButton!
+    
     @IBOutlet weak var card: UIView!
     
     @IBOutlet weak var prevButton: UIButton!
@@ -32,6 +34,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var wrongButton2: UIButton!
     @IBOutlet weak var wrongButton: UIButton!
     @IBOutlet weak var rightButton: UIButton!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //First start with the flashcard invisible and slightly smaller in size
+        card.alpha = 0.0
+        card.transform = CGAffineTransform.identity.scaledBy(x: 0.75, y: 0.75)
+        
+        rightButton.alpha = 0.0
+        rightButton.transform = CGAffineTransform.identity.scaledBy(x: 0.75, y: 0.75)
+        
+        wrongButton.alpha = 0.0
+        wrongButton.transform = CGAffineTransform.identity.scaledBy(x: 0.75, y: 0.75)
+        
+        wrongButton2.alpha = 0.0
+        wrongButton2.transform = CGAffineTransform.identity.scaledBy(x: 0.75, y: 0.75)
+        
+        // Animation
+        UIView.animate(withDuration: 0.6, delay: 0.5, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: []) {
+            self.card.alpha = 1.0
+            self.card.transform = CGAffineTransform.identity
+            
+            self.rightButton.alpha = 1.0
+            self.rightButton.transform = CGAffineTransform.identity
+            
+            self.wrongButton2.alpha = 1.0
+            self.wrongButton2.transform = CGAffineTransform.identity
+            
+            self.wrongButton.alpha = 1.0
+            self.wrongButton.transform = CGAffineTransform.identity
+        }
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,67 +101,162 @@ class ViewController: UIViewController {
         }
     }
     
+    
     @IBAction func didTapOnReset(_ sender: Any) {
         if questionLabel.isHidden == true{
             questionLabel.isHidden = false
         }
-        if wrongButton2.isHidden == true{
-            wrongButton2.isHidden = false
+        if wrongButton2.isEnabled == false{
+            wrongButton2.isEnabled = true
         }
-        if wrongButton.isHidden == true{
-            wrongButton.isHidden = false
+        if wrongButton.isEnabled == false{
+            wrongButton.isEnabled = true
+        }
+        if rightButton.isEnabled == false{
+            rightButton.isEnabled = true
         }
     }
     
     @IBAction func onTapOption2(_ sender: Any) {
-        questionLabel.isHidden = true
+        if rightButton == correctAnswerButton{
+            flipFlashcard()
+        }
+        else{
+            questionLabel.isHidden = false
+            rightButton.isEnabled = false
+        }
     }
     @IBAction func onTapOption3(_ sender: Any) {
-        wrongButton.isHidden = true
+        if wrongButton == correctAnswerButton {
+            flipFlashcard()
+        }
+        else{
+            questionLabel.isHidden = false
+            wrongButton.isEnabled = false
+        }
     }
     @IBAction func onTapOption1(_ sender: Any) {
-        wrongButton2.isHidden = true
+        if wrongButton2 == correctAnswerButton {
+            flipFlashcard()
+        }
+        else {
+            questionLabel.isHidden = false
+            wrongButton2.isEnabled = false
+        }
     }
     
     @IBAction func didTapOnNext(_ sender: Any) {
         //increase current index
         currentIndex += 1
         
-        //update labels
-        updateLabels()
-        
         //update buttons
         updateNextPrevButtons()
+        
+        animateCardOut()
+        
+        if questionLabel.isHidden == true{
+            questionLabel.isHidden = false
+        }
+        if wrongButton2.isEnabled == false{
+            wrongButton2.isEnabled = true
+        }
+        if wrongButton.isEnabled == false{
+            wrongButton.isEnabled = true
+        }
+        if rightButton.isEnabled == false{
+            rightButton.isEnabled = true
+        }
     }
     
     @IBAction func didTapOnPrev(_ sender: Any) {
         currentIndex -= 1
         
-        updateLabels()
-        
         updateNextPrevButtons()
+        
+        animateCardIn()
+        
+        if questionLabel.isHidden == true{
+            questionLabel.isHidden = false
+        }
+        if wrongButton2.isEnabled == false{
+            wrongButton2.isEnabled = true
+        }
+        if wrongButton.isEnabled == false{
+            wrongButton.isEnabled = true
+        }
+        if rightButton.isEnabled == false{
+            rightButton.isEnabled = true
+        }
     }
     
     @IBAction func onClick(_ sender: Any) {
-        if questionLabel.isHidden == false {
-            questionLabel.isHidden = true
+        flipFlashcard()
+        if wrongButton2.isEnabled == false{
+            wrongButton2.isEnabled = true
         }
-        else if (questionLabel.isHidden == true){
-            questionLabel.isHidden = false
+        if wrongButton.isEnabled == false{
+            wrongButton.isEnabled = true
+        }
+        if rightButton.isEnabled == false{
+            rightButton.isEnabled = true
         }
     }
+    
+    func flipFlashcard() {
+       UIView.transition(with: card, duration: 0.3, options: .transitionFlipFromRight) {
+        if self.questionLabel.isHidden == false {
+            self.questionLabel.isHidden = true
+            }
+        else if (self.questionLabel.isHidden == true){
+            self.questionLabel.isHidden = false
+            }
+        }
+    }
+    
+    func animateCardOut(){
+        UIView.animate(withDuration: 0.3) {
+            self.card.transform = CGAffineTransform.identity.translatedBy(x: -300.0, y: 0.0)
+        } completion: { (finished) in
+            //Update labels
+            self.updateLabels()
+            
+            self.animateCardIn()
+        }
+
+    }
+    
+    func animateCardIn(){
+        // Start on the right side (don't animate this)
+        card.transform = CGAffineTransform.identity.translatedBy(x: 300.0, y: 0.0)
+        
+        UIView.animate(withDuration: 0.3) {
+            self.card.transform = CGAffineTransform.identity
+        }
+        self.updateLabels()
+    }
+    
     func updateLabels(){
         //get current flashcard
         if currentIndex <= flashcards.count - 1 && currentIndex >= 0 {
             let currentFlashcard = flashcards[currentIndex]
             
-        
             //update labels
             questionLabel.text = currentFlashcard.question
             answerLabel.text = currentFlashcard.answer
-            wrongButton.setTitle(currentFlashcard.extraAnswerOne, for: .normal)
-            wrongButton2.setTitle(currentFlashcard.extraAnswerTwo, for: .normal)
-            rightButton.setTitle(currentFlashcard.answer, for: .normal)
+            
+            let buttons = [wrongButton2, rightButton, wrongButton].shuffled()
+            
+            let answers = [currentFlashcard.answer, currentFlashcard.extraAnswerOne, currentFlashcard.extraAnswerTwo].shuffled()
+            
+            for (button, answer) in zip(buttons, answers) {
+                // Set the title of this random button, with a random answer
+                button?.setTitle(answer, for: .normal)
+                
+                // If this is the correct answer save the button
+                if answer == currentFlashcard.answer {
+                    correctAnswerButton = button
+                }
+            }
         }
     }
     
